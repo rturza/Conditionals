2eaccf7347807b444bb768739c3f6ff2bf97ac74
Turza <cse.turza@gmail.com>
2022-07-04 10:11:49 +0600

    statements in a line

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -12,11 +12,14 @@ namespace Conditionals
         {
             bool isGoldCustomer = true;
 
-            float price;
-            if (isGoldCustomer)
-                price = 19.95f;
-            else
-                price = 29.95f;
+            //float price;
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
+            //Console.WriteLine(price);
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
             Console.WriteLine(price);
         }
     }
