01028d1334e8608d1f93320133573c3572d4eb5e
Turza <cse.turza@gmail.com>
2022-07-04 10:07:37 +0600

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGoldCustomer = true;
+
+            float price;
+            if (isGoldCustomer)
+                price = 19.95f;
+            else
+                price = 29.95f;
+            Console.WriteLine(price);
+        }
+    }
+}
